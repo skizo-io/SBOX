8e54a91703d77a638cf128f2cda7a35dda9955f8
kimbyungkyu <>
2019-12-15 20:43:11 +0900

    library code

--- app/src/main/java/skizo/sbox/preferences/AppPreference.kt
+++ app/src/main/java/skizo/sbox/preferences/AppPreference.kt
@@ -7,11 +7,14 @@ class AppPreference: BasePreferences() {
 
     companion object {
 
+        /*
         var mainNavigationSelectId = R.id.navigation_home
             get() = base.get("mainNavigationSelectId", R.id.navigation_home)
             set(value) {
                 field = base.set("mainNavigationSelectId", value)
             }
+            
+         */
 
 
     }
